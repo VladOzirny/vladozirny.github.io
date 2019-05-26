1a673a42926b735fa34a9ef8f80a52f14433cfd9
VladOzirny <perseverance.vlad@gmail.com>
2019-05-26 13:05:17 +0300

    Add contact form

--- js/common.js
+++ js/common.js
@@ -16,59 +16,39 @@ $(".toggle-mnu").click(function() {
 });
 
 
+//Contact form
 
+$('input').focus(function(){
+  $(this).parents('.form-group').addClass('focused');
+});
+
+$('input').blur(function(){
+  var inputValue = $(this).val();
+  if ( inputValue == "" ) {
+    $(this).removeClass('filled');
+    $(this).parents('.form-group').removeClass('focused');  
+  } else {
+    $(this).addClass('filled');
+  }
+})  
+$('textarea').focus(function(){
+  $(this).parents('.form-group').addClass('focused');
+});
+
+$('textarea').blur(function(){
+  var inputValue = $(this).val();
+  if ( inputValue == "" ) {
+    $(this).removeClass('filled');
+    $(this).parents('.form-group').removeClass('focused');  
+  } else {
+    $(this).addClass('filled');
+  }
+})  
 
 
 // End
 
-// Replace all SVG images with inline SVG
 
-$('img.svg').each(function(){
-	var $img = $(this);
-	var imgID = $img.attr('id');
-	var imgClass = $img.attr('class');
-	var imgURL = $img.attr('src');
 
-	$.get(imgURL, function(data) {
-		// Get the SVG tag, ignore the rest
-		var $svg = $(data).find('svg');
-
-		// Add replaced image's ID to the new SVG
-		if(typeof imgID !== 'undefined') {
-			$svg = $svg.attr('id', imgID);
-		}
-		// Add replaced image's classes to the new SVG
-		if(typeof imgClass !== 'undefined') {
-			$svg = $svg.attr('class', imgClass+' replaced-svg');
-		}
-
-		// Remove any invalid XML tags as per http://validator.w3.org
-		$svg = $svg.removeAttr('xmlns:a');
-
-		// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-		if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-			$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-		}
-
-		// Replace image with new SVG
-		$img.replaceWith($svg);
-
-	}, 'xml');
-
-});
-
-//Replace SVG END
-
-/*
-if ($(window).width() < 600) {
-   alert('Less than 600');
-   //$(".interest").css(
-    //{'background-color': 'rgba(0, 0, 0, 0.820)', 'box-shadow': '0px 0px 20px #000000'}
-//);
-}
-else {
-   alert('More than 1400');
-}
-*/
 
 });
